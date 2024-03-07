85b3b113749d3def27cfab020c370a20c4047784
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-07 15:55:01 +0400

    generate archive

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,17 @@
 #!/usr/bin/env bash
 #  Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
+apt-get -y update
+apt-get -y install nginx
+sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default
+sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default
+sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default
+# echo "Ceci n'est pas une page" > /var/www/html/custom_404.html
+service nginx start
 mkdir -p /data/web_static/releases/test/
-
-touch /data/web_static/releases/test/index.html
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >> /data/web_static/releases/test/index.html
-
+mkdir -p /data/web_static/shared/
+echo "Hello Holberton School!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu: /data/
-
-print %s "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	server_name _;
-	add_header X-Served-By $HOSTNAME;
-
-	location /hbnb_static/ {
-		alias /data/web_static/current/hbnb_static/;
-		index index.html;
-	}
-}" > /etc/nginx/sites-enabled/default
-
+chown -R ubuntu:ubuntu /data/
+sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
 service nginx restart
+exit 0
