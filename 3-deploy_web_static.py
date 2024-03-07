d2639af53ed50dde948af549f86a929aebfc2da5
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-07 22:24:07 +0400

    deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-"""
-This for deploy web static
-"""
+""" this for deploy the web"""
 
 from fabric.api import *
 import os
+from datetime import datetime
+import tarfile
 
 env.hosts = ['100.25.34.17', '34.229.56.85']
 env.user = "ubuntu"
 
 
-def do_deploy(archive_path):
-    """ Deploys archive to servers"""
-    if not os.path.exists(archive_path):
+def deploy():
+    """ deploy the archive"""
+    tar = do_pack()
+    if not tar:
         return False
+    return do_deploy(tar)
 
-    results = []
 
-    res = put(archive_path, "/tmp")
-    results.append(res.succeeded)
-
-    basename = os.path.basename(archive_path)
-    if basename[-4:] == ".tgz":
-        name = basename[:-4]
-    newdir = "/data/web_static/releases/" + name
-    run("mkdir -p " + newdir)
-    run("tar -xzf /tmp/" + basename + " -C " + newdir)
-
-    run("rm /tmp/" + basename)
-    run("mv " + newdir + "/web_static/* " + newdir)
-    run("rm -rf " + newdir + "/web_static")
-    run("rm -rf /data/web_static/current")
-    run("ln -s " + newdir + " /data/web_static/current")
+def do_pack():
+    """ do archive"""
+    savedir = "versions/"
+    filename = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+    if not os.path.exists(savedir):
+        os.mkdir(savedir)
+    with tarfile.open(savedir + filename, "w:gz") as tar:
+        tar.add("web_static", arcname=os.path.basename("web_static"))
+    if os.path.exists(savedir + filename):
+        return savedir + filename
+    else:
+        return None
 
 
 def do_deploy(archive_path):
@@ -56,7 +54,5 @@ def do_deploy(archive_path):
     run("rm -rf " + newdir + "/web_static")
     run("rm -rf /data/web_static/current")
     run("ln -s " + newdir + " /data/web_static/current")
-
-    return True
 
     return True
