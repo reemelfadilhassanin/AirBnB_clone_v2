335d81a69949de7bc779a6c7b709d1413226a573
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-23 04:59:52 +0400

    render template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -55,7 +55,7 @@ def number_template(n):
     Display a HTML page only if n is an integer.
 
     """
-    return render_template('5-number.html', number=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
