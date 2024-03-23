59ca89cd0d610a032fb69b00ab75af336b51e456
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-23 15:56:31 +0400

    improve the engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,27 +32,14 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
+        """reload file path to JSON file path
+        """
         try:
-            temp = {}
-            with open(self.__file_path, 'r') as f:
-                temp = json.load(f)
-            for key, val in temp.items():
-                self.all()[key] = classes[val['__class__']](**val)
-        except:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
@@ -61,18 +48,6 @@ class FileStorage:
             key_delete = obj.__class__.__name__ + '.' + obj.id
             if key_delete in self.__objects:
                 del self.__objects[key_delete]
-
-
-def reload(self):
-    """reloado JSON file path
-    """
-    try:
-        with open(self.__file_path, 'r', encoding="UTF-8") as f:
-            for key, value in (json.load(f)).items():
-                value = eval(value["__class__"])(**value)
-                self.__objects[key] = value
-    except FileNotFoundError:
-        pass
 
 
 def close(self):
