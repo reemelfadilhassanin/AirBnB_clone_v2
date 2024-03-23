c1dfb58665f534df5b488d23eed26720d97dc6f5
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-23 15:10:00 +0400

    evevn or mod

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -59,5 +59,13 @@ def number_template(n):
     return render_template('5-number.html', number=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    Display a HTML page only if n isodd or even
+    """
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
